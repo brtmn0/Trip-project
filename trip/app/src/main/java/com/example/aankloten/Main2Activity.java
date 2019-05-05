54773a6fb441bd48c04623aa3c5786f25e071e3b
WesleyVanTurnhout <wvanturnhout@hotmail.com>
2019-05-05 23:34:04 +0200

    Remove more Legacy
    Remove old menu drawers.
    Add placeholder actions for navbar onclick items.

--- trip/app/src/main/java/com/example/aankloten/Main2Activity.java
+++ trip/app/src/main/java/com/example/aankloten/Main2Activity.java
@@ -80,17 +80,19 @@ public class Main2Activity extends AppCompatActivity
         // Handle navigation view item clicks here.
         int id = item.getItemId();
 
-        if (id == R.id.nav_camera) {
-            // Handle the camera action
-        } else if (id == R.id.nav_gallery) {
+        if (id == R.id.home) {
 
-        } else if (id == R.id.nav_slideshow) {
+        } else if (id == R.id.makememory) {
 
-        } else if (id == R.id.nav_manage) {
+        } else if (id == R.id.memories) {
 
-        } else if (id == R.id.nav_share) {
+        } else if (id == R.id.maps) {
 
-        } else if (id == R.id.nav_send) {
+        } else if (id == R.id.friends) {
+
+        } else if (id == R.id.account) {
+
+        } else if (id == R.id.setting) {
 
         }
 

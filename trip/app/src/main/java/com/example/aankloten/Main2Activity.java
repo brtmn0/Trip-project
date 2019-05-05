4a0bb885c4bde175c4248f600dd46dc27a6e0cc6
WesleyVanTurnhout <wvanturnhout@hotmail.com>
2019-05-05 20:47:45 +0200

    Fixed correct navbar
    The corret navbar now shows (including Make Memory, Memories and Friends.)

--- trip/app/src/main/java/com/example/aankloten/Main2Activity.java
+++ trip/app/src/main/java/com/example/aankloten/Main2Activity.java
@@ -19,7 +19,7 @@ public class Main2Activity extends AppCompatActivity
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main2);
+        setContentView(R.layout.content_main2);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 

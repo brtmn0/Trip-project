580f14273349adc53acee2eb293bc7a120b5ff91
raygun333 <raygun333@users.noreply.github.com>
2019-06-12 16:46:51 +0200

    LayoutVoorEditen
    Added a layout for the editing part in the picture.

--- trip/app/src/main/java/com/example/aankloten/FullImageActivity.java
+++ trip/app/src/main/java/com/example/aankloten/FullImageActivity.java
@@ -3,10 +3,15 @@ package com.example.aankloten;
 import android.net.Uri;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.ImageView;
+import android.widget.ImageButton;
+import android.widget.FrameLayout;
 
 public class FullImageActivity extends AppCompatActivity {
     ImageView fullImage;
+    ImageButton arrowUp;
+    FrameLayout infoFrame;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -14,8 +19,15 @@ public class FullImageActivity extends AppCompatActivity {
         setContentView(R.layout.activity_full_image);
         fullImage = (ImageView)findViewById(R.id.full_image);
 
+
         String data = getIntent().getExtras().getString("img");
 
         fullImage.setImageURI(Uri.parse(data));
+    }
+    public void openLayout(View v){
+        arrowUp = (ImageButton)findViewById(R.id.openButton);
+        infoFrame = (FrameLayout)findViewById(R.id.infoFrame);
+        arrowUp.setVisibility(View.INVISIBLE);
+        infoFrame.setVisibility(View.VISIBLE);
     }
 }
